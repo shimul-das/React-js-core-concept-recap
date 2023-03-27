ae99a810a4515248790a638e6a61dc6e102b5f05
Shimul Das <shimul.191002110@gmail.com>
2023-03-28 01:06:30 +0600

    48_5-3 Load data using the useEffect and display data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
+      <Dataload></Dataload>
       <District name="Noyakhli" speciality="Vibhag"></District>
       <District name="Bhola" speciality="Nature of beauty"></District>
       <District name="Shylet" speciality="Nature of hill"></District>
@@ -32,6 +33,32 @@ function District(props){
       <h4>Speciality:{props.speciality}</h4>
       <h4>Powe: {power}</h4>
       <button onClick={boostPower}>Boost The powe</button>
+    </div>
+  )
+}
+
+function Dataload(){
+  const [posts,setPosts]=useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res=>res.json())
+    .then(data=>setPosts(data))
+    // .then(data=>console.log(data))
+  },[])
+  return(
+    <div>
+<h1>Posts:{posts.length}</h1>
+{
+  posts.map(post=><Post title={post.title} body={post.body}></Post>)
+}
+    </div>
+  )
+}
+function Post(props){
+  return(
+    <div className='post'>
+      <h2>Title:{props.title}</h2>
+      <p>Title:{props.body}</p>
     </div>
   )
 }
